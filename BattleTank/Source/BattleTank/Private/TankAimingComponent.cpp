32afcf5e6a39e366e023d207fa3eb4fb6df197e1
Aldwulf <aldwulf13@gmail.com>
2018-09-01 06:54:44 +0100

    Lecture 148. Using FMath::Clamp(). Barrel pitching up and down!

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -84,5 +84,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 
 	///UE_LOG(LogTemp, Warning, TEXT("DeltaRotator: %s"), *DeltaRotator.ToString())
 
-	Barrel->Elevate(5);
+	Barrel->Elevate(DeltaRotator.Pitch);
 }
